b2fefadd9b8fbfc776a21767b61c8c58fd5a43f0
HedvigM <hedvig@mejstedt.se>
2022-01-18 16:20:26 +0100

    feature: creating the store.

---
+++ frontend/src/reducers/member.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const member = createSlice({
+  name: 'member',
+  initialState: {
+    memberId: null,
+    name: null,
+    accessToken: null
+  },
+  reducers: {
+    setMemberId: (store, action) => {
+      store.memberId = action.payload;
+    },
+    setMemberName: (store, action) => {
+      store.name = action.payload;
+    },
+    setaccessToken: (store, action) => {
+      store.accessToken = action.payload;
+    }
+  }
+});
+
+export default member;
