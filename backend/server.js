267fc26e9715ce25ceb01f3fade7974d45204000
HedvigM <hedvig@mejstedt.se>
2022-01-18 11:39:01 +0100

    feature: creating new members.
    The backend for the possibillity to create a new member is now running.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import crypto from 'crypto';
+import bcrypt from 'bcrypt';
 
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/session';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -9,9 +11,62 @@ mongoose.Promise = Promise;
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+// add the middleware damien shows...
 app.use(cors());
 app.use(express.json());
+
+const MemberSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+});
+
+const Member = mongoose.model('Member', MemberSchema);
+
+app.get('/', (req, res) => {
+  res.send('Hello Music Lovers!');
+});
+
+app.post('/signup', async (req, res) => {
+  const { name, password } = req.body;
+
+  // I will need to explain this more to my future self.
+  try {
+    const salt = bcrypt.genSaltSync();
+
+    if (password.length < 5) {
+      throw 'Password must be at least 5 characters long';
+    }
+
+    const newMember = await new Member({
+      name,
+      password: bcrypt.hashSync(password, salt)
+    }).save();
+
+    res.status(201).json({
+      response: {
+        memberId: newMember._id,
+        name: newMember.name,
+        accessToken: newMember.accessToken
+      },
+      success: true
+    });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ response: 'Could not create member', success: false });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
