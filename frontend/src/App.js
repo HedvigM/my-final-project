475e6c1b8f55b3042e90b56134713fb1a91738ea
HedvigM <hedvig@mejstedt.se>
2022-01-22 12:03:32 +0100

    fix: cleaning up app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import './App.css';
-/* import styled from 'styled-components'; */
 import { Provider } from 'react-redux';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-/* import { Header } from './components/Header'; */
 import { Login } from './components/Login';
-/* import { ProfileScreenTwo } from './Screens/ProfileScreenTwo'; */
 import { SearchScreen } from './Screens/SearchScreen';
-
-/* import { MyTunes } from './components/MyTunes'; */
-/* import { Footer } from './components/Footer'; */
 import { NotFound } from './components/NotFound';
 import member from './reducers/member';
 
@@ -25,7 +19,6 @@ const App = () => {
     <Provider store={store}>
       <BrowserRouter>
         <div>
-          {/* <Header /> */}
           <div>
             <Routes>
               <Route path="/" element={<SearchScreen />} />
@@ -33,7 +26,6 @@ const App = () => {
               <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
-          {/* <Footer /> */}
         </div>
       </BrowserRouter>
     </Provider>
@@ -41,23 +33,3 @@ const App = () => {
 };
 
 export default App;
-
-/* const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-
-  min-height: 100vh;
-`;
-const InnerContainer = styled.div`
-  min-width: 334px;
-  max-width: 500px;
-  margin: 0 auto;
-  height: 100%; */
-/* background-color: #ff885e; */
-/* border: 3px solid red;
-  @media (min-width: 0px) and (max-width: 767px) {
-    min-width: 200px;
-    max-width: 300px;
-  }
-`; */
