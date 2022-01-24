4045df0194948245d9c416f98f6954117e24b88d
HedvigM <hedvig@mejstedt.se>
2022-01-24 21:16:32 +0100

    Update SearchTunes.js

--- frontend/src/components/SearchTunes.js
+++ frontend/src/components/SearchTunes.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
-import ReactPaginate from 'react-paginate';
 import styled from 'styled-components';
 import { POPULAR_URL } from '../utils/url';
 
