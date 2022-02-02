14ce76e89a6e47809665bd4f1b85831a6de83e5c
HedvigM <hedvig@mejstedt.se>
2022-02-02 09:54:01 +0100

    Creating DetailedMember.js

---
+++ frontend/src/components/DetailMember.js
@@ -0,0 +1,16 @@
+import React /* , { useEffect, useState }  */ from 'react';
+import styled from 'styled-components';
+import { API_URL } from '../utils/url';
+
+/* import { useSelector } from 'react-redux'; */
+
+export const DetailMember = (member) => {
+  /* const [loading, setLoading] = useState(false);
+  const [list, setList] = useState([]); */
+
+  return <Container></Container>;
+};
+
+const Container = styled.div`
+  color: black;
+`;
