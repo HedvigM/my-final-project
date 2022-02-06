3776f754301ed44c23528e957ea2b6dc44751662
HedvigM <hedvig@mejstedt.se>
2022-02-06 20:10:34 +0100

    feature: responsive design.

--- frontend/src/components/Following.js
+++ frontend/src/components/Following.js
@@ -2,10 +2,13 @@ import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { API_URL } from '../utils/url';
+import styled from 'styled-components';
 
 export const Following = () => {
   const [followingMember, setFollowingMember] = useState([]);
   const memberId = useSelector((store) => store.member.memberId);
+  const relations = useSelector((store) => store.relations.relations);
+  console.log(relations);
 
   useEffect(() => {
     fetch(API_URL('relations'))
@@ -16,14 +19,18 @@ export const Following = () => {
   }, []);
 
   return (
-    <>
-      <h1>People i'm following:</h1>
+    <Text>
+      <h1>The members i'm following are:</h1>
       {followingMember.map(
         (item, index) =>
-          item.following._id === memberId && (
-            <p key={index}>{item.followed.memberName}</p>
-          )
+          item.following === memberId && <p key={index}>{item.followed}</p>
       )}
-    </>
+    </Text>
   );
 };
+
+const Text = styled.div`
+  h1 {
+    font-size: 1.17em;
+  }
+`;
