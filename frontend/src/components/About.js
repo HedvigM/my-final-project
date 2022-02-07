b401582d5443872ff3969487ad7fdb1f0e5226b5
HedvigM <hedvig@mejstedt.se>
2022-02-07 17:44:16 +0100

    feature: added some about text.

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
 export const About = () => {
-  return <h3>About</h3>;
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        Hey! 👋 My name is Hedvig and I made this app. It’s for all of us that
+        some times experience difficulties common up with tunes to start at the
+        session. The idea is to save all the tunes you know, and the ones you
+        want to learn. Then go to other members to se what tunes you have in
+        common. 🎻
+      </p>
+    </>
+  );
 };
