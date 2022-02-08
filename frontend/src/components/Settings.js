478bce6b8f9b3a804d819edb085a215712d2de99
HedvigM <hedvig@mejstedt.se>
2022-02-08 20:49:10 +0100

    fix: removing inputs thats not needed.

--- frontend/src/components/Settings.js
+++ frontend/src/components/Settings.js
@@ -85,35 +85,38 @@ export const Settings = () => {
 
   return (
     <>
-      <h1>{memberName}</h1>
-      <h2>{town}</h2>
-      <input
-        id="town"
-        type="text"
-        placeholder="In which town do you live?"
-        value={town}
-        /* onChange={(event) => setTown(event.target.value)} */
-      ></input>
-      <h2>{email}</h2>
-      <input
-        id="email"
-        type="email"
-        placeholder="Email"
-        value={email}
-        /* onChange={(event) => setEmail(event.target.value)} */
-      ></input>
       <p>{profileText}</p>
-      <button onClick={logout}>Log out</button>
-      <Delete
+      <Btn onClick={logout}>Log out</Btn>
+      <Btn
+        delete
         onClick={() => {
           onDelete(memberId);
         }}>
         Delete account
-      </Delete>
+      </Btn>
     </>
   );
 };
 
-const Delete = styled.button`
-  color: red;
+const Btn = styled.button`
+  background-color: ${(props) => (props.delete ? '#fc6666' : 'white')};
+
+  color: black;
+  margin: 3px;
+  padding: 3px;
+  font-size: 15px;
+  border-radius: 4px;
+  border: none;
+  transition-duration: 0.2s;
+  box-shadow: none;
+  font-family: var(--button-font);
+  white-space: nowrap;
+
+  :hover {
+    background-color: white;
+    color: var(--secondary-color);
+    transition-duration: 0.2s;
+    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
+      0 17px 50px 0 rgba(0, 0, 0, 0.19);
+  }
 `;
