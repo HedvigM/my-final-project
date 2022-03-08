193619074e9758ddbf8d1b77188712a34b93e180
HedvigM <hedvig@mejstedt.se>
2022-03-08 21:20:11 +0100

    fix: remove bad code and swedish.

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -5,18 +5,14 @@ export const About = () => {
   return (
     <>
       <H1>About</H1>
-      <br />
-      <br />
 
       <h2>Hey! 👋</h2>
-      <br />
-      <br />
       <p>
         My name is Hedvig and I made this app. It’s for all of us that some
         times experience difficulties common up with tunes to start at the
         session.
-        <br />
-        <br />
+      </p>
+      <p>
         The idea is to save all the tunes you know, and the ones you want to
         learn. Then go to other members to se what tunes you have in common. 🎻
       </p>
@@ -25,7 +21,7 @@ export const About = () => {
 };
 const H1 = styled.h1`
   text-align: center;
-  /* Liten Dator - */
+  /* small laptop - */
   @media (min-width: 992px) {
     font-size: 3em;
   }
