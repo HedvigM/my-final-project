710319a7c952f39dc28e225aefa7da713f767c1b
HedvigM <hedvig@mejstedt.se>
2022-03-29 22:19:49 +0200

    feature: created the button component

---
+++ frontend/src/components/styledComponents/Buttons.js
@@ -0,0 +1,95 @@
+import styled from 'styled-components';
+
+export const Btn = styled.button`
+  background-color: ${(props) =>
+    props.follow ? 'var(--main-color)' : 'white'};
+
+  color: black;
+  margin: 3px;
+
+  padding: 3px;
+  font-size: 15px;
+  border-radius: 4px;
+  border: none;
+  transition-duration: 0.2s;
+  box-shadow: none;
+  font-family: var(--button-font);
+  white-space: nowrap;
+
+  :hover {
+    background-color: white;
+    color: var(--secondary-color);
+    transition-duration: 0.2s;
+    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
+      0 17px 50px 0 rgba(0, 0, 0, 0.19);
+  }
+`;
+
+export const BtnTunes = styled.button`
+  background-color: var(--main-color);
+  color: black;
+  margin: 3px;
+  width: 70px;
+  padding: 3px;
+  font-size: 15px;
+  border-radius: 4px;
+  border: none;
+  transition-duration: 0.2s;
+  box-shadow: none;
+  font-family: var(--button-font);
+  white-space: nowrap;
+
+  :hover {
+    background-color: white;
+    color: var(--secondary-color);
+    transition-duration: 0.2s;
+    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
+      0 17px 50px 0 rgba(0, 0, 0, 0.19);
+  }
+`;
+
+export const LogOutBtn = styled.button`
+  background-color: ${(props) => (props.delete ? '#fc6666' : 'white')};
+
+  color: black;
+  margin: 3px;
+  padding: 3px;
+  font-size: 15px;
+  border-radius: 4px;
+  border: none;
+  transition-duration: 0.2s;
+  box-shadow: none;
+  font-family: var(--button-font);
+  white-space: nowrap;
+
+  :hover {
+    background-color: white;
+    color: var(--secondary-color);
+    transition-duration: 0.2s;
+    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
+      0 17px 50px 0 rgba(0, 0, 0, 0.19);
+  }
+`;
+
+export const DeleteBtn = styled.button`
+  background-color: ${(props) => (props.delete ? '#fc6666' : 'white')};
+
+  color: black;
+  margin: 3px;
+  padding: 3px;
+  font-size: 15px;
+  border-radius: 4px;
+  border: none;
+  transition-duration: 0.2s;
+  box-shadow: none;
+  font-family: var(--button-font);
+  white-space: nowrap;
+
+  :hover {
+    background-color: white;
+    color: var(--secondary-color);
+    transition-duration: 0.2s;
+    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
+      0 17px 50px 0 rgba(0, 0, 0, 0.19);
+  }
+`;
