da8f305cc92377b1a829e91b7a74e3daa9bd976d
HedvigM <hedvig@mejstedt.se>
2022-04-12 20:06:32 +0200

    styled button

--- frontend/src/components/login/LoginAuth0.js
+++ frontend/src/components/login/LoginAuth0.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { LogInBtn } from '../styledComponents/Buttons';
 
 export const LoginAuth0 = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <LogInBtn onClick={() => loginWithRedirect()}>Log In</LogInBtn>;
 };
