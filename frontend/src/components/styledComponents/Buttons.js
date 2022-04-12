10098fdfaf07ea4f0d578a7d5be1600129b95871
HedvigM <hedvig@mejstedt.se>
2022-04-12 20:06:45 +0200

    styled login button

--- frontend/src/components/styledComponents/Buttons.js
+++ frontend/src/components/styledComponents/Buttons.js
@@ -48,6 +48,29 @@ export const BtnTunes = styled.button`
   }
 `;
 
+export const LogInBtn = styled.button`
+  background-color: var(--secondary-color);
+
+  color: white;
+  margin: 3px;
+  padding: 5px 10px;
+  font-size: 15px;
+  border-radius: 4px;
+  border: none;
+  transition-duration: 0.2s;
+  box-shadow: none;
+  font-family: var(--button-font);
+  white-space: nowrap;
+
+  :hover {
+    background-color: white;
+    color: var(--secondary-color);
+    transition-duration: 0.2s;
+    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
+      0 17px 50px 0 rgba(0, 0, 0, 0.19);
+  }
+`;
+
 export const LogOutBtn = styled.button`
   background-color: ${(props) => (props.delete ? '#fc6666' : 'white')};
 
